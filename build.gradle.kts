1466e4cce6219ed7f28f9794e07230a10b1e354d
junghyunkee <jhkman1208@gmail.com>
2023-05-03 18:49:38 +0900

    .gitignore is now working

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.callsign"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
 	compileOnly {
